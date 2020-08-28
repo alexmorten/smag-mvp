f9f9dc909f6bc660cd02acfe591c84b195e800f2
alexmorten <alexandrius.martinus@gmail.com>
2020-08-29 01:55:00 +0200

    fix path issues

--- insta/db/start-postgres.sh
+++ insta/db/start-postgres.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "# MIGRATE DATABASE"
-/migrate -database "postgres://postgres:12345678@postgres:5432/${POSTGRES_DB}?sslmode=disable" -path debezium/migrations up
+/migrate -database "postgres://postgres:12345678@postgres:5432/${POSTGRES_DB}?sslmode=disable" -path db/migrations up
 
 pwd
 
-ls -a 
+ls -a
 
 echo "# PREPARE DEBEZIUM"
 curl -i -X POST -H "Accept:application/json" \
